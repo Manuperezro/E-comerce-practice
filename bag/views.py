e0cef027968123413d9117d640bc928f8d6616ae
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-06-05 17:39:12 +0000

    card adding products

--- bag/views.py
+++ bag/views.py
@@ -12,8 +12,8 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    bag = request.sesion.get('bag', {})
-    
+    bag = request.session.get('bag', {})
+
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
@@ -22,3 +22,4 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+    
