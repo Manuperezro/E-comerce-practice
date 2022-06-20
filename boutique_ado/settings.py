52cd6721d8de8698f575034d7d1b01008ea0a088
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-06-20 21:17:56 +0000

    ws

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -166,7 +166,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -177,6 +176,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'e-commerce-ado'
     AWS_S3_REGION_NAME = 'us-east-1'
